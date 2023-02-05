048d40693ed05ba3d7df1f1564875d3b4c53dbe9
LimonBlaze <1195384759@qq.com>
2023-02-06 03:15:56 +0800

    Remove redundant network stuffs

--- src/main/java/plus/dragons/createcentralkitchen/entry/CckPackets.java
+++ src/main/java/plus/dragons/createcentralkitchen/entry/CckPackets.java
@@ -11,20 +11,14 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 import plus.dragons.createcentralkitchen.CentralKitchen;
-import plus.dragons.createcentralkitchen.content.contraptions.components.stove.BlazeStoveEditPacket;
-import plus.dragons.createcentralkitchen.content.contraptions.components.stove.CookingGuideEditPacket;
 
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static net.minecraftforge.network.NetworkDirection.PLAY_TO_SERVER;
-
 public enum CckPackets {
-
     // Client to Server
-    CONFIGURE_COOKING_GUIDE_FOR_BLAZE(CookingGuideEditPacket.class, CookingGuideEditPacket::new, PLAY_TO_SERVER),
-    CONFIGURE_BLAZE_STOVE(BlazeStoveEditPacket.class, BlazeStoveEditPacket::new, PLAY_TO_SERVER);
+    ;
 
     public static final ResourceLocation CHANNEL_NAME = CentralKitchen.genRL("main");
     public static final int NETWORK_VERSION = 2;
@@ -49,19 +43,24 @@ public enum CckPackets {
     }
 
     public static void sendToNear(Level world, BlockPos pos, int range, Object message) {
-        channel.send(
-                PacketDistributor.NEAR.with(PacketDistributor.TargetPoint.p(pos.getX(), pos.getY(), pos.getZ(), range, world.dimension())),
-                message);
+        channel.send(PacketDistributor.NEAR.with(
+            PacketDistributor.TargetPoint.p(
+                pos.getX(),
+                pos.getY(),
+                pos.getZ(),
+                range,
+                world.dimension())),
+            message);
     }
 
     private static class LoadedPacket<T extends SimplePacketBase> {
         private static int index = 0;
 
-        private BiConsumer<T, FriendlyByteBuf> encoder;
-        private Function<FriendlyByteBuf, T> decoder;
-        private BiConsumer<T, Supplier<NetworkEvent.Context>> handler;
-        private Class<T> type;
-        private NetworkDirection direction;
+        private final BiConsumer<T, FriendlyByteBuf> encoder;
+        private final Function<FriendlyByteBuf, T> decoder;
+        private final BiConsumer<T, Supplier<NetworkEvent.Context>> handler;
+        private final Class<T> type;
+        private final NetworkDirection direction;
 
         private LoadedPacket(Class<T> type, Function<FriendlyByteBuf, T> factory, NetworkDirection direction) {
             encoder = T::write;
@@ -79,4 +78,5 @@ public enum CckPackets {
                     .add();
         }
     }
+    
 }
