e66549277f8e34cac7e1520bdfb263ecd18b82b8
LimonBlaze <1195384759@qq.com>
2023-02-17 18:50:08 +0800

    Migrate recipes to use tags

--- src/main/java/plus/dragons/createcentralkitchen/data/recipe/FarmersDelightRecipes.java
+++ src/main/java/plus/dragons/createcentralkitchen/data/recipe/FarmersDelightRecipes.java
@@ -1,16 +1,14 @@
 package plus.dragons.createcentralkitchen.data.recipe;
 
-import com.simibubi.create.AllFluids;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.content.contraptions.processing.HeatCondition;
 import com.simibubi.create.foundation.data.recipe.CreateRecipeProvider.GeneratedRecipe;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
-import plus.dragons.createcentralkitchen.data.tag.CentralKitchenTags;
 import plus.dragons.createcentralkitchen.modules.farmersdelight.entry.FdFluids;
 import vectorwing.farmersdelight.common.registry.ModItems;
+import vectorwing.farmersdelight.common.tag.ForgeTags;
 
 @MethodsReturnNonnullByDefault
 public class FarmersDelightRecipes extends RecipeGen {
@@ -23,8 +21,8 @@ public class FarmersDelightRecipes extends RecipeGen {
         .requiresHeat(HeatCondition.HEATED)),
     HOT_COCOA_MIXING = modded(mixing("hot_cocoa")
         .output(FdFluids.HOT_COCOA.get(), 250)
-        .require(CentralKitchenTags.fluid(new ResourceLocation("forge", "milk")), 125)
-        .require(CentralKitchenTags.fluid(new ResourceLocation("forge", "chocolate")), 125)
+        .require(AllTags.forgeFluidTag("milk"), 125)
+        .require(AllTags.forgeFluidTag("chocolate"), 125)
         .requiresHeat(HeatCondition.HEATED)),
     MELON_JUICE_MIXING = modded(mixing("melon_juice")
         .output(FdFluids.MELON_JUICE.get(), 250)
@@ -35,8 +33,8 @@ public class FarmersDelightRecipes extends RecipeGen {
         .require(Items.SUGAR)),
     TOMATO_SAUCE_MIXING = modded(mixing("tomato_sauce")
         .output(FdFluids.TOMATO_SAUCE.get(), 250)
-        .require(ModItems.TOMATO.get())
-        .require(ModItems.TOMATO.get())
+        .require(ForgeTags.VEGETABLES_TOMATO)
+        .require(ForgeTags.VEGETABLES_TOMATO)
         .requiresHeat(HeatCondition.HEATED)),
     COOKIE_COMPACTING = common(compacting("cookie")
         .output(Items.COOKIE, 8)
@@ -50,7 +48,7 @@ public class FarmersDelightRecipes extends RecipeGen {
         .require(AllTags.forgeItemTag("flour/wheat"))),
     HONEY_COOKIE_COMPACTING = modded(compacting("honey_cookie")
         .output(ModItems.HONEY_COOKIE.get(), 8)
-        .require(AllFluids.HONEY.get(), 250)
+        .require(AllTags.forgeFluidTag("honey"), 250)
         .require(AllTags.forgeItemTag("flour/wheat"))
         .require(AllTags.forgeItemTag("flour/wheat"))),
     FINISH = null;
