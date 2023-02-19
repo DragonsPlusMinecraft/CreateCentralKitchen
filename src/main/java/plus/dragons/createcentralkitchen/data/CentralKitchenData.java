71984c4ccb01ff87ea453a2c2711f0e20d454016
LimonBlaze <1195384759@qq.com>
2023-02-19 11:36:34 +0800

    Fix typo

--- src/main/java/plus/dragons/createcentralkitchen/data/CentralKitchenData.java
+++ src/main/java/plus/dragons/createcentralkitchen/data/CentralKitchenData.java
@@ -12,6 +12,8 @@ import plus.dragons.createcentralkitchen.data.recipe.FarmersRespiteRecipes;
 import plus.dragons.createcentralkitchen.data.tag.CentralKitchenTags;
 import plus.dragons.createcentralkitchen.modules.farmersdelight.foundation.ponder.FdPonderIndex;
 import plus.dragons.createcentralkitchen.modules.farmersdelight.foundation.ponder.FdPonderTag;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.foundation.ponder.FrPonderIndex;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.foundation.ponder.FrPonderTag;
 import plus.dragons.createdragonlib.lang.LangFactory;
 
 public enum CentralKitchenData {
@@ -22,8 +24,10 @@ public enum CentralKitchenData {
             return;
         LangFactory langFactory = LangFactory.create(CentralKitchen.NAME, CentralKitchen.ID)
                 .ponders(() -> {
-                    FdPonderIndex.register();
                     FdPonderTag.register();
+                    FdPonderIndex.register();
+                    FrPonderTag.register();
+                    FrPonderIndex.register();
                 })
                 .ui();
         modBus.addListener(EventPriority.LOWEST, langFactory::datagen);
@@ -34,8 +38,8 @@ public enum CentralKitchenData {
     @SubscribeEvent(priority = EventPriority.LOW)
     public void afterRegistrate(final GatherDataEvent event) {
         DataGenerator datagen = event.getGenerator();
-        datagen.addProvider(true,new FarmersDelightRecipes(datagen));
-        datagen.addProvider(true,new FarmersRespiteRecipes(datagen));
+        datagen.addProvider(true, new FarmersDelightRecipes(datagen));
+        datagen.addProvider(true, new FarmersRespiteRecipes(datagen));
     }
     
 }
