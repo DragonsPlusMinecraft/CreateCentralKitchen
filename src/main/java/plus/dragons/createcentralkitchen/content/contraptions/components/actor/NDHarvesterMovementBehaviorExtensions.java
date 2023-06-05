3071cd6ad3abd90f6c27f669f93f252d12d3353c
MarbleGate <marblegatekeeper@outlook.com>
2023-06-05 14:46:58 +0800

    Fix Fungus colonies won't be replanted. Close #52

--- src/main/java/plus/dragons/createcentralkitchen/content/contraptions/components/actor/NDHarvesterMovementBehaviorExtensions.java
+++ src/main/java/plus/dragons/createcentralkitchen/content/contraptions/components/actor/NDHarvesterMovementBehaviorExtensions.java
@@ -55,13 +55,15 @@ public class NDHarvesterMovementBehaviorExtensions {
         if (!partial && age < colony.getMaxAge())
             return;
         Level level = context.world;
-        if (partial && replant) {
-            behaviour.dropItem(context, new ItemStack(colony.fungusType.get(), age));
+        if (replant) {
             level.playSound(null, pos, SoundEvents.MOOSHROOM_SHEAR, SoundSource.BLOCKS, 1.0F, 1.0F);
             level.setBlock(pos, state.setValue(ageProp, 0), 2);
         } else {
-            BlockHelper.destroyBlock(level, pos, 1, stack -> behaviour.dropItem(context, stack));
+            BlockHelper.destroyBlock(level, pos, 1, $ -> {});
         }
+        behaviour.dropItem(context, age < colony.getMaxAge()?
+                new ItemStack(colony.fungusType.get(), age):
+                new ItemStack(colony.asItem()));
     }
     
     private static final BooleanProperty PEARL = BooleanProperty.create("pearl");
