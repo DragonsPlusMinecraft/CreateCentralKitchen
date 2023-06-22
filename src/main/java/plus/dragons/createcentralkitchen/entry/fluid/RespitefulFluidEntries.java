d309d1d9c9fe07fec595be17f92cc7d990330fd7
MarbleGate <marblegatekeeper@outlook.com>
2023-06-23 07:02:57 +0800

    Update Respiteful compat, more liquid & recipe.

--- src/main/java/plus/dragons/createcentralkitchen/entry/fluid/RespitefulFluidEntries.java
+++ src/main/java/plus/dragons/createcentralkitchen/entry/fluid/RespitefulFluidEntries.java
@@ -6,11 +6,10 @@ import plus.dragons.createcentralkitchen.foundation.utility.ModLoadSubscriber;
 import plus.dragons.createcentralkitchen.foundation.utility.Mods;
 import plus.dragons.respiteful.entries.RespitefulItems;
 
-import static plus.dragons.createcentralkitchen.foundation.fluid.VirtualFluidFromItem.virtual;
+import static plus.dragons.createcentralkitchen.foundation.fluid.VirtualFluidFromItem.*;
 
 @ModLoadSubscriber(modid = Mods.RESPITEFUL)
 public class RespitefulFluidEntries {
-    
     public static final FluidEntry<VirtualFluid>
         GREEN_TEA_ICE_CREAM = virtual(RespitefulItems.GREEN_TEA_ICE_CREAM,
             Mods.respiteful("block/green_tea_ice_cream_block"), 500).register(),
@@ -19,6 +18,8 @@ public class RespitefulFluidEntries {
         BLACK_TEA_ICE_CREAM = virtual(RespitefulItems.BLACK_TEA_ICE_CREAM,
             Mods.respiteful("block/black_tea_ice_cream_block"), 500).register(),
         COFFEE_ICE_CREAM = virtual(RespitefulItems.COFFEE_ICE_CREAM,
-            Mods.respiteful("block/coffee_ice_cream_block"), 500).register();
+            Mods.respiteful("block/coffee_ice_cream_block"), 500).register(),
+        ADZUKI_MILK_TEA = watery(RespitefulItems.ADZUKI_MILK_TEA, 0xa5575b).register(),
+        MOCHA_COFFEE = milky(RespitefulItems.MOCHA_COFFEE, 0x653f2f).register();
     
 }
