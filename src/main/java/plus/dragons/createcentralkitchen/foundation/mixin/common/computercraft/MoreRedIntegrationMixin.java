372c23bec3f93c60fe0f029f71307e39f8834d94
MarbleGate <marblegatekeeper@outlook.com>
2024-02-05 13:26:05 +0800

    Fix Crash with CC:Tweaked Close #69, Close #74, Close #75

---
+++ src/main/java/plus/dragons/createcentralkitchen/foundation/mixin/common/computercraft/MoreRedIntegrationMixin.java
@@ -0,0 +1,21 @@
+package plus.dragons.createcentralkitchen.foundation.mixin.common.computercraft;
+
+import dan200.computercraft.shared.integration.MoreRedIntegration;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import plus.dragons.createcentralkitchen.api.block.entity.DelegatingSmartTileEntity;
+
+@Mixin(MoreRedIntegration.class)
+public class MoreRedIntegrationMixin {
+    @Inject(method = "attachBlockCapabilities", at = @At("HEAD"), cancellable = true)
+    private static void injected(AttachCapabilitiesEvent<BlockEntity> event, CallbackInfo ci) {
+        Object var2 = event.getObject();
+        if(var2 instanceof DelegatingSmartTileEntity){
+            ci.cancel();
+        }
+    }
+}
