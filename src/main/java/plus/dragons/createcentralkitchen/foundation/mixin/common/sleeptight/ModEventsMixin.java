2f27b5765007298c4920626d33a86b394ab91cd1
MarbleGate <marblegatekeeper@outlook.com>
2025-09-06 07:17:06 +0800

    Fix crash with Sleep Tight mod

---
+++ src/main/java/plus/dragons/createcentralkitchen/foundation/mixin/common/sleeptight/ModEventsMixin.java
@@ -0,0 +1,19 @@
+package plus.dragons.createcentralkitchen.foundation.mixin.common.sleeptight;
+
+import net.mehvahdjukaar.sleep_tight.core.ModEvents;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import plus.dragons.createcentralkitchen.api.block.entity.DelegatingSmartTileEntity;
+
+@Mixin(value = ModEvents.class, remap = false)
+public class ModEventsMixin {
+    @Inject(method = "shouldHaveBedData", at = @At("HEAD"), cancellable = true)
+    private static void injected(BlockEntity blockEntity, CallbackInfoReturnable<Boolean> cir) {
+        if(blockEntity instanceof DelegatingSmartTileEntity){
+            cir.setReturnValue(false);
+        }
+    }
+}
