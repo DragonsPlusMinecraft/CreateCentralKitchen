3a3a68769c24f9dcc06b2c9fad3c29ea355a3eef
MarbleGate <marblegatekeeper@outlook.com>
2026-08-11 08:25:53 +0800

    [fix] Restore recipe unlock advancements

--- src/main/java/plus/dragons/createcentralkitchen/foundation/resource/BuiltinResourceGameTests.java
+++ src/main/java/plus/dragons/createcentralkitchen/foundation/resource/BuiltinResourceGameTests.java
@@ -101,10 +101,35 @@ public class BuiltinResourceGameTests {
             CentralKitchen.genRL("filling/creamy_corn_drink"),
             CentralKitchen.genRL("mixing/corn_soup"),
             CentralKitchen.genRL("mixing/creamy_corn_drink"));
+    private static final List<ResourceLocation> ATMOSPHERIC_ADVANCEMENTS = List.of(
+            CentralKitchen.genRL("recipes/crafting/aloe_gel_block_from_bucket"),
+            CentralKitchen.genRL("recipes/crafting/aloe_gel_bottles_from_bucket"),
+            CentralKitchen.genRL("recipes/crafting/aloe_gel_bucket"),
+            CentralKitchen.genRL("recipes/crafting/aloe_gel_bucket_from_block"));
+    private static final List<ResourceLocation> AUTUMNITY_ADVANCEMENTS = List.of(
+            CentralKitchen.genRL("recipes/crafting/pancake_from_dough"),
+            CentralKitchen.genRL("recipes/crafting/sap_bottles_from_bucket"),
+            CentralKitchen.genRL("recipes/crafting/sap_bucket_from_bottles"),
+            CentralKitchen.genRL("recipes/crafting/sappy_maple_log_from_sap_bucket"),
+            CentralKitchen.genRL("recipes/crafting/sappy_maple_wood_from_sap_bucket"),
+            CentralKitchen.genRL("recipes/crafting/syrup_bottles_from_bucket"),
+            CentralKitchen.genRL("recipes/crafting/syrup_bucket_from_bottles"),
+            CentralKitchen.genRL("recipes/misc/syrup_bucket_from_sap_bucket_campfire"),
+            CentralKitchen.genRL("recipes/misc/syrup_bucket_from_sap_bucket_smelting"),
+            CentralKitchen.genRL("recipes/misc/syrup_bucket_from_sap_bucket_smoking"));
+    private static final List<ResourceLocation> UPGRADE_AQUATIC_ADVANCEMENTS = List.of(
+            CentralKitchen.genRL("recipes/crafting/mulberry_jam_block_from_bucket"),
+            CentralKitchen.genRL("recipes/crafting/mulberry_jam_bottles_from_bucket"),
+            CentralKitchen.genRL("recipes/crafting/mulberry_jam_bucket"),
+            CentralKitchen.genRL("recipes/crafting/mulberry_jam_bucket_from_block"));
     private static final Map<String, List<ResourceLocation>> BUILTIN_RECIPES = Map.of(
             Mods.AD, ABNORMALS_DELIGHT_RECIPES,
             Mods.ATMOSPHERIC, ATMOSPHERIC_RECIPES);
     private static final Map<String, List<ResourceLocation>> RESTORED_RECIPES = new LinkedHashMap<>();
+    private static final Map<String, List<ResourceLocation>> RESTORED_ADVANCEMENTS = Map.of(
+            Mods.ATMOSPHERIC, ATMOSPHERIC_ADVANCEMENTS,
+            Mods.AUTUMNITY, AUTUMNITY_ADVANCEMENTS,
+            Mods.UA, UPGRADE_AQUATIC_ADVANCEMENTS);
 
     static {
         RESTORED_RECIPES.put(Mods.FD, FARMERS_DELIGHT_RECIPES);
@@ -136,6 +161,14 @@ public class BuiltinResourceGameTests {
             for (ResourceLocation recipeId : recipeIds)
                 helper.assertTrue(recipes.byKey(recipeId).isPresent(),
                         "The " + modId + " built-in pack did not load " + recipeId);
+        });
+        var advancements = helper.getLevel().getServer().getAdvancements();
+        RESTORED_ADVANCEMENTS.forEach((modId, advancementIds) -> {
+            if (!Mods.isLoaded(modId))
+                return;
+            for (ResourceLocation advancementId : advancementIds)
+                helper.assertTrue(advancements.getAdvancement(advancementId) != null,
+                        "The " + modId + " built-in pack did not load " + advancementId);
         });
         helper.succeed();
     }
