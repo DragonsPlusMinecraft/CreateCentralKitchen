ce728d39a3c8b2c6456e13a1cb97a82b7c08268e
MarbleGate <marblegatekeeper@outlook.com>
2026-08-19 09:27:13 +0800

    [fix] Support End's Delight 2.6.1

--- src/main/java/plus/dragons/createcentralkitchen/entry/fluid/EDFluidEntries.java
+++ src/main/java/plus/dragons/createcentralkitchen/entry/fluid/EDFluidEntries.java
@@ -2,7 +2,7 @@ package plus.dragons.createcentralkitchen.entry.fluid;
 
 import static plus.dragons.createcentralkitchen.CentralKitchen.REGISTRATE;
 
-import cn.foggyhillside.ends_delight.registry.ItemRegistry;
+import cn.foggyhillside.ends_delight.registry.ModItems;
 import com.simibubi.create.AllFluids;
 import com.simibubi.create.content.fluids.VirtualFluid;
 import com.simibubi.create.foundation.data.CreateRegistrate;
@@ -26,15 +26,15 @@ import plus.dragons.createcentralkitchen.foundation.utility.Mods;
 public class EDFluidEntries {
     private static final ResourceLocation BUBBLE_TEA_STILL = CentralKitchen.genRL("fluid/bubble_tea_still");
     private static final ResourceLocation BUBBLE_TEA_FLOW = CentralKitchen.genRL("fluid/bubble_tea_flow");
-    public static final FluidEntry<VirtualFluid> DRAGONS_BREATH_SODA = VirtualFluidFromItem.watery(ItemRegistry.DragonBreathSoda, 0xd16191)
+    public static final FluidEntry<VirtualFluid> DRAGONS_BREATH_SODA = VirtualFluidFromItem.watery(ModItems.DRAGON_BREATH_SODA, 0xd16191)
             .register(),
-            CHORUS_FLOWER_TEA = VirtualFluidFromItem.watery(ItemRegistry.ChorusFlowerTea, 0xa27da1)
+            CHORUS_FLOWER_TEA = VirtualFluidFromItem.watery(ModItems.CHORUS_FLOWER_TEA, 0xa27da1)
                     .register(),
-            CHORUS_FRUIT_WINE = VirtualFluidFromItem.watery(ItemRegistry.ChorusFruitWine, 0xd39cd3)
+            CHORUS_FRUIT_WINE = VirtualFluidFromItem.watery(ModItems.CHORUS_FRUIT_WINE, 0xd39cd3)
                     .register(),
-            CHORUS_FRUIT_MILK_TEA = VirtualFluidFromItem.milky(ItemRegistry.ChorusFruitMilkTea, 0xffa3ff)
+            CHORUS_FRUIT_MILK_TEA = VirtualFluidFromItem.milky(ModItems.CHORUS_FRUIT_MILK_TEA, 0xffa3ff)
                     .register(),
-            CHORUS_FRUIT_BUBBLE_TEA = bubbleTea("chorus_fruit_bubble_tea", ItemRegistry.BubbleTea, 0xffa3ff, 250)
+            CHORUS_FRUIT_BUBBLE_TEA = bubbleTea("chorus_fruit_bubble_tea", ModItems.BUBBLE_TEA, 0xffa3ff, 250)
                     .register();
 
     public static FluidBuilder<VirtualFluid, CreateRegistrate> bubbleTea(String name, RegistryObject<? extends ItemLike> container, int colorIn, int amount) {
